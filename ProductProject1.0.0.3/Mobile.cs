4d54380f461f44057503bb6781fec384e90d4fc0
Ganesh Gurkhude <97801118+ganeshydv@users.noreply.github.com>
2022-10-26 22:44:03 +0530

    all c sharp files

---
+++ ProductProject1.0.0.3/Mobile.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product3_0_dll
+{
+    public class Mobile:Product
+    {
+
+        public override string Display()
+        {
+            string msg = base.Display();
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append(msg);
+            if (Is5G)
+            {
+                return sb.Append("it is 5G mobile.\n") + "";
+            }else return base.Display();
+            
+        }
+        private bool m_is5G;
+        public bool Is5G
+        {
+            get { return m_is5G; }
+            set
+            {
+                m_is5G = value;
+            }
+        }
+        private string m_OS;
+        private string OS
+        {
+            get { 
+                return m_OS; 
+            }
+            set
+            {
+                m_OS=value;
+            }
+        }
+
+    }
+}
